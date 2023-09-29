3df3fb8cefb673ca70dbc560bc344fbd66fa604c
Shreya-sanjeev <78650465+Shreya-sanjeev@users.noreply.github.com>
2023-09-29 23:14:47 +0530

    Add files via upload

---
+++ persondetect.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import imutils
+
+protopath = "D:\crowd count new\MobileNetSSD_deploy.prototxt"
+modelpath = "D:\crowd count new\MobileNetSSD_deploy.caffemodel"
+detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
+
+CLASSES = ["background", "aroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
+           "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+
+
+def main():
+    image = cv2.imread('img8.jpg')
+    image = imutils.resize(image, width=600)
+    peopleCount = []
+    (H, W) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(image, 0.007843, (W, H), 127.5)
+    detector.setInput(blob)
+    person_detections = detector.forward()
+    for i in np.arange(0, person_detections.shape[2]):
+
+        confidence = person_detections[0, 0, i, 2]
+        if confidence > 0.5:
+
+            idx = int(person_detections[0, 0, i, 1])
+            if CLASSES[idx] != "person":
+                continue
+
+            person_box = (person_detections[0, 0, i, 3:7]*np.array([W, H, W, H]))
+
+            (startX, startY, endX, endY) = person_box.astype("int")
+
+            # cv2.rectangle(image,(startX,startY),(endX,endY),(0,0,255),2)
+            cv2.rectangle(image, (startX, startY),
+                          (endX, endY), (0, 0, 255), 2)
+            peopleCount.append(i)
+
+    cv2.imshow("Results", image)
+    cv2.waitKey(0)
+ 
+    return peopleCount
+
+    # print("people count = " + str(len(np.array)))
+
+    # print("people count" + str(len()))
+    cv2.destroyAllWindows()
+
+
+print('people count ', main()[-1])#+1
